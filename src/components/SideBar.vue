e65abba741139ab6600abd0a4807988b8d34b01b
李方傑 <lifangjie@ajiedeMacBook-Air.local>
2022-02-07 23:16:29 +0800

    sideBar:feat set default

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,6 +1,35 @@
 <template>
   <!-- 阿傑負責 -->
-  <div>side bar</div>
+  <section class="sideBar">
+    <div class="sideBar_main">
+      <div class="userInfo">
+        <div class="userInfo_icon">
+          <img src="@/assets/avatar.svg" alt="avatar-icon" />
+        </div>
+        <span class="userInfo_name">號稱165的大叔</span>
+        <span class="userInfo_floor">樓主</span>
+      </div>
+      <div class="sideBar_main_title">
+        <h4>狼若回頭, 不是報恩就是報仇</h4>
+        <span class="date">06-09-2055</span>
+        <span class="time">00:00</span>
+      </div>
+      <div class="sideBar_main_content">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad adipisci,
+          placeat ullam cum vitae dignissimos cumque voluptatem nobis sunt modi
+          reiciendis! Laboriosam, aliquam. Voluptas quos quo natus minima,
+          laborum provident.
+        </p>
+      </div>
+      <div class="sideBar_main_others">
+        <font-awesome-icon :icon="['fas', 'bookmark']" />
+        <font-awesome-icon :icon="['fas', 'link']" />
+        <font-awesome-icon :icon="['far', 'thumbs-up']" />
+        <font-awesome-icon :icon="['fas', 'comment-dots']" />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
--- src/plugins/font-awesome.js
+++ src/plugins/font-awesome.js
@@ -3,18 +3,23 @@ import {
   faCalendarAlt,
   faListAlt,
   faFileAlt,
+  fas,
 } from "@fortawesome/free-solid-svg-icons";
 import {
   faCalendarAlt as farFaCalendarAlt,
   faListAlt as farFaListAlt,
   faFileAlt as farFileAlt,
+  far,
 } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(faCalendarAlt);
 library.add(faListAlt);
 library.add(faFileAlt);
+library.add(fas);
+
 library.add(farFaCalendarAlt);
 library.add(farFaListAlt);
 library.add(farFileAlt);
+library.add(far);
 export default FontAwesomeIcon;
