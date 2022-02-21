64f2701e14a66fde879931791eb016da9f7a0e0b
Hoo <sherryho148@gmail.com>
2022-02-21 17:11:18 +0800

    develop: feat add favorite page loading card effect

--- src/components/tabs/Favorite.vue
+++ src/components/tabs/Favorite.vue
@@ -1,30 +1,40 @@
 <template>
   <div class="cards_wrapper">
-    <Card
-      v-for="card in favoriteList"
-      :key="card.wanna_know_id"
-      :card="card"
-      @setFavoriteList="setFavoriteList"
-    ></Card>
+    <template v-if="isLoading">
+      <LoadingCard v-for="card in loadingCardCout" :key="card"></LoadingCard>
+    </template>
+    <template v-else>
+      <Card
+        v-for="card in favoriteList"
+        :key="card.wanna_know_id"
+        :card="card"
+        @setFavoriteList="setFavoriteList"
+      ></Card>
+    </template>
   </div>
   <Pagination @changePage="changePage"></Pagination>
 </template>
 
 <script>
 import Card from "@/components/Card.vue";
+import LoadingCard from "@/components/LoadingCard.vue";
 import Pagination from "@/components/Pagination.vue";
 
 export default {
-  components: { Card, Pagination },
+  components: { Card, Pagination, LoadingCard },
   data() {
     return {
       favoriteList: [],
       currentPage: 1,
+      isLoading: true,
+      loadingCardCout: JSON.parse(localStorage.getItem("favoriteList")).length,
     };
   },
   methods: {
     getLocalStorage() {
-      return JSON.parse(localStorage.getItem("favoriteList")) || [];
+      this.isLoading = true;
+      this.favoriteList =
+        JSON.parse(localStorage.getItem("favoriteList")) || [];
     },
     setFavoriteList(favoriteList) {
       this.favoriteList = favoriteList;
@@ -33,8 +43,11 @@ export default {
       this.currentPage = page;
     },
   },
-  created() {
-    this.favoriteList = this.getLocalStorage();
+  async created() {
+    await this.getLocalStorage();
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 300);
   },
 };
 </script>
