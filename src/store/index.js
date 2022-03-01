9e2a6c8d65e93b9a64c5845712f8696de4d0b473
Hoo <sherryho148@gmail.com>
2022-03-01 20:11:04 +0800

    develop:feat add cardID and get wannaKnow comments from api

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import { createStore } from "vuex";
+import api from "@/service/api";
 
 export default createStore({
   state: {
     isSideBarOpen: false,
     searchWord: "",
     isSearch: false,
+    cardID: "",
+    commentsData: [],
   },
   mutations: {
     toggleSideBar(state, status) {
@@ -17,7 +20,31 @@ export default createStore({
     toggleSearch(state) {
       state.isSearch = false;
     },
+    changeCardID(state, wanna_know_id) {
+      state.cardID = wanna_know_id;
+    },
+    getComments(state, comments) {
+      state.commentsData = comments;
+    },
   },
-  actions: {},
+  actions: {
+    changeCardID({ state, dispatch }, wanna_know_id) {
+      console.log(state, dispatch);
+      state.cardID = wanna_know_id;
+      dispatch("getComments", wanna_know_id);
+    },
+    getCardComments({ commit, state }, wanna_know_id) {
+      console.log(wanna_know_id);
+      if (wanna_know_id === state.cardID) {
+        console.log("一樣的cardID");
+        return;
+      } else {
+        commit("changeCardID", wanna_know_id);
+        api
+          .getComments(wanna_know_id)
+          .then((res) => commit("getComments", res.data));
+      }
+    },
+  },
   modules: {},
 });
